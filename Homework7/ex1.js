b913c7a23ed9c4fde7625d50747dec5cf181e2ff
ChoppaSxdd <72573679+ChoppaSxd@users.noreply.github.com>
2022-08-27 10:31:03 +0300

    Add files via upload

---
+++ Homework7/ex1.js
@@ -0,0 +1,11 @@
+let newWindow = window.open("", "", "width=300,height=300,left=100,top=100");
+
+setTimeout(function() {
+    newWindow.resizeTo(500, 500);
+    setTimeout(function() {
+        newWindow.moveTo(200, 200);
+        setTimeout(function() {
+            newWindow.close();
+        }, 2000);
+    }, 2000);
+}, 2000);
